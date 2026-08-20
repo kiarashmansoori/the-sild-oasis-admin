35b00c75998afd7d822bd84035a399db1ce59352
kiarash mansoori <kiarash.sr2@gmail.com>
2026-08-20 16:56:40 +0430

    update auth

--- components/Login/LoginForm.jsx
+++ components/Login/LoginForm.jsx
@@ -5,8 +5,8 @@ import FormRow from "../FormRow";
 import { useAuth } from "@/src/features/auth/useAuth";
 
 function LoginForm() {
-  const [email, setEmail] = useState("Kiarashcoii@gmail.com");
-  const [password, setPassword] = useState("11111111");
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
   const { Login, isPending } = useAuth();
   function handleSubmit(e) {
     e.preventDefault();
