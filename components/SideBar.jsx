78c983d50a7a0a9d865f4f59d76a485b5187630a
kiarash mansoori <kiarash.sr2@gmail.com>
2026-08-20 15:29:58 +0430

    update admin

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -1,13 +1,11 @@
 import Logo from "./Logo";
 import Navbar from "./Navbar";
-import SeedButton from "./SeedButton";
 
 function SideBar() {
   return (
     <aside className=" row-span-full  py-9 border-r border-zinc-100 dark:border-zinc-600 dark:bg-zinc-900 space-y-7 ">
       <Logo />
       <Navbar />
-      <SeedButton />
     </aside>
   );
 }
